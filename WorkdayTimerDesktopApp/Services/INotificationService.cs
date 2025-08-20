423d7259597fa18dcdc233a3384ca99fd0d87693
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-08-20 04:24:57 -0300

    Add - NotifyIcon service

---
+++ WorkdayTimerDesktopApp/Services/INotificationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkdayTimerDesktopApp.Services;
+public enum NotificationType
+{
+    TimerComplete,
+    StopTimer,
+    PauseOnCoffeeBreakTimer,
+    PauseNonOnCoffeeBreakTimer
+}
+
+public interface INotificationService
+{
+    event Action<NotificationType> NotificationAccepted;
+
+    void Notify(string title, string message, int timeout, NotificationType notificationType, ToolTipIcon icon);
+}
---
+++ WorkdayTimerDesktopApp/Services/NotifyIconNotificationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkdayTimerDesktopApp.Services;
+public class NotifyIconNotificationService : INotificationService
+{
+    private readonly NotifyIcon _notifyIcon;
+
+    private NotificationType _lastNotificationType;
+
+    public event Action<NotificationType> NotificationAccepted;
+
+    public NotifyIconNotificationService(NotifyIcon notifyIcon)
+    {
+        _notifyIcon = notifyIcon;
+
+        _notifyIcon.BalloonTipClicked += NotifyIcon_BalloonTipClicked;
+    }
+
+    public void Notify(string title, string message, int timeout, NotificationType notificationType, ToolTipIcon icon)
+    {
+        _lastNotificationType = notificationType;
+        _notifyIcon.ShowBalloonTip(timeout, title, message, icon);
+    }
+
+    private void NotifyIcon_BalloonTipClicked(object sender, EventArgs e)
+    {
+        NotificationAccepted?.Invoke(_lastNotificationType);
+    }
+}
